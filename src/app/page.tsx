5f844843a9bcf2238a5d40999294cdac1d832ef7
yeasinarafatprantik2002 <yeasinarafatprantik@gmail.com>
2024-02-25 23:21:35 +0600

    add WhyChoseUs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChoseUs from "@/components/WhyChoseUs";
 
 const page = () => {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
+      <WhyChoseUs />
     </main>
   );
 };
